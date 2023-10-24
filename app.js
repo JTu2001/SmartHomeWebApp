b99cb94e0224f9a3b28ad618004e4c1104c4bbb6
TUNJIC Josip <josip.tunjic77@gmail.com>
2023-10-24 20:28:28 +0200

    Created Simple App

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.json({info: 'Smart Home App'}));
+
+app.get('/getVoltage', (req, res) => {
+    res.send('3,3V');
+});
+
+app.get('/getTemperature', (req, res) => {
+    res.send('28,5°C');
+});
+
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+module.exports = server;
